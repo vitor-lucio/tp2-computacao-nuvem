098af083d8fe47a70b227aebf93fd07a4c015602
vitor-lucio <33261657+vitor-lucio@users.noreply.github.com>
2023-10-21 20:20:10 -0300

    make client communicate with server over POST

--- client/main.py
+++ client/main.py
@@ -1,13 +1,15 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from dotenv import load_dotenv
 import os
+import requests
 
 app = Flask(__name__)
 load_dotenv(override=True)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>" + str(os.getenv('TESTE'))
+    response = requests.post("http://{}:{}/api/recommend".format(os.getenv('SERVER_NAME'), os.getenv('SERVER_PORT')), json={'corpo': 'corpo da requisicao'})
+    return "mensagem do cliente, com uma variavel de teste <{}> e a resposta do servidor  <{}>".format(os.getenv('TESTE'), response.text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=30501,debug=True)
--- server/main.py
+++ server/main.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
 load_dotenv(override=True)
 
-@app.route("/api/recommend")
+@app.route("/api/recommend", methods=['POST'])
 def hello_world():
-    return "<p>Hello, World!</p>" + str(os.getenv('TESTE'))
+    request_json_body = request.get_json(force=True)
+    return "<p>Hello, World!</p> {} {}".format(os.getenv('TESTE'), request_json_body)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=30502,debug=True)
