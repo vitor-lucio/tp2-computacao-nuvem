d6d8a77e13c6fce7a2be0f453364454f96f6d2df
vitor-lucio <33261657+vitor-lucio@users.noreply.github.com>
2023-10-22 18:26:48 -0300

    fpgrowth using samples ds1 and ds2

--- server/main.py
+++ server/main.py
@@ -13,45 +13,26 @@ playlists = [] #lista de objetos representando as playlists, contendo playlist i
 # atraves de arquivos csv, como os samples de playlist, vai ser criado a lista para geracao das regras de recomendacao e a lista com objetos identificando as playlists e suas musicas
 previous_pid = 0
 playlist_songs = []
-# with open('playlist-sample-ds1.csv','r', encoding="utf8") as data:
-#    for line in csv.reader(data):
-#         if line[0] != 'pid':
-#             current_pid = line[0]
-#             if current_pid == previous_pid:
-#                 playlists[-1]['songs'].append(line[5])
+with open('playlist-sample-ds1.csv','r', encoding="utf8") as data:
+   for line in csv.reader(data):
+        if line[0] != 'pid':
+            current_pid = line[0]
+            if current_pid == previous_pid:
+                playlists[-1]['songs'].append(line[5])
 
-#                 playlist_songs.append(line[5])
-#             else:
-#                 if len(playlist_songs) != 0:
-#                     itemSetList.append(playlist_songs)
+                playlist_songs.append(line[5])
+            else:
+                if len(playlist_songs) != 0:
+                    itemSetList.append(playlist_songs)
                 
-#                 playlists.append({'pid': line[0], 'songs': [line[5]]})
+                playlists.append({'pid': line[0], 'songs': [line[5]]})
 
-#                 playlist_songs = [line[5]]
-#                 previous_pid = current_pid
-
-# previous_pid = 0
-# playlist_songs = []
-# with open('playlist-sample-ds2.csv','r', encoding="utf8") as data:
-#    for line in csv.reader(data):
-#         if line[0] != 'pid':
-#             current_pid = line[0]
-#             if current_pid == previous_pid:
-#                 playlists[-1]['songs'].append(line[5])
-
-#                 playlist_songs.append(line[5])
-#             else:
-#                 if len(playlist_songs) != 0:
-#                     itemSetList.append(playlist_songs)
-                
-#                 playlists.append({'pid': line[0], 'songs': [line[5]]})
-
-#                 playlist_songs = [line[5]]
-#                 previous_pid = current_pid
+                playlist_songs = [line[5]]
+                previous_pid = current_pid
 
 previous_pid = 0
 playlist_songs = []
-with open('playlist-sample-ds1-smaller-copy.csv','r', encoding="utf8") as data:
+with open('playlist-sample-ds2.csv','r', encoding="utf8") as data:
    for line in csv.reader(data):
         if line[0] != 'pid':
             current_pid = line[0]
@@ -79,9 +60,9 @@ with open('playlist-sample-ds1-smaller-copy.csv','r', encoding="utf8") as data:
 #             ['eggs', 'bacon', 'apple'],
 #             ['soup', 'bacon', 'banana']]
 
-freqItemSet, rules = fpgrowth(itemSetList, minSupRatio=0.2, minConf=0.1)
-# print("----------------------------------------------------- frequent item set -----------------------------------------------------")
-# print(freqItemSet)
+freqItemSet, rules = fpgrowth(itemSetList, minSupRatio=0.015, minConf=0.6)
+print("----------------------------------------------------- frequent item set -----------------------------------------------------")
+print(freqItemSet)
 print("----------------------------------------------------- rules -----------------------------------------------------------------")
 print(rules)
 
