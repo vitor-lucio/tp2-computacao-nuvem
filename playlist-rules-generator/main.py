d2551737be8b58496fc99488b4aae1bfe00c19be
vitor-lucio <33261657+vitor-lucio@users.noreply.github.com>
2023-11-26 15:35:38 -0300

    update playlist rules generator to use new datasets and stop sending model directly to server folder

--- playlist-rules-generator/main.py
+++ playlist-rules-generator/main.py
@@ -13,67 +13,42 @@ itemSetList = [] # lista de listas de track_names (nome das musicas) de cada pla
 playlists = [] # lista de objetos representando as playlists, contendo playlist id (pid) e musicas (songs). Necessario para identificar e analisar cada playlist de acordo com as regras geradas para recomendacao
 
 # atraves de arquivos csv, como os samples de playlist, vai ser criado a lista para geracao das regras de recomendacao e a lista com objetos identificando as playlists e suas musicas
-previous_pid = 0
-playlist_songs = []
-with open('playlist-sample-ds1.csv','r', encoding="utf8") as data:
-   for line in csv.reader(data):
-        if line[0] != 'pid':
-            current_pid = line[0]
-            if current_pid == previous_pid:
-                playlists[-1]['songs'].append(line[5])
-
-                playlist_songs.append(line[5])
-            else:
-                if len(playlist_songs) != 0:
-                    itemSetList.append(playlist_songs)
-                
-                playlists.append({'pid': line[0], 'songs': [line[5]]})
-
-                playlist_songs = [line[5]]
-                previous_pid = current_pid
-
 # previous_pid = 0
 # playlist_songs = []
-# with open('playlist-sample-ds2.csv','r', encoding="utf8") as data:
-#    for line in csv.reader(data):
-#         if line[0] != 'pid':
-#             current_pid = line[0]
-#             if current_pid == previous_pid:
-#                 playlists[-1]['songs'].append(line[5])
+with open('2023_spotify_ds1.csv','r', encoding="utf8") as data: # mudar arquivo csv para '2023_spotify_ds2.csv' quando for testar a atualizacao do model
+   for line in csv.reader(data):
+        if line[6] != 'pid':
+            new_playlist = True
+            for playlist in playlists:
+                if playlist['pid'] == line[6]: # line[6] e a coluna pid do dataset
+                    playlist['songs'].append(line[7]) # line[7] e a coluna track_name do dataset
+                    new_playlist = False
+           
+            if new_playlist: # Se a playlist da linha do dataset não estiver na lista de playlists, criar novo dicionario para esta playlist.
+                playlists.append({'pid': line[6], 'songs': [line[7]]})
+            new_playlist = True
+           
 
-#                 playlist_songs.append(line[5])
-#             else:
-                
-#                 playlists.append({'pid': line[0], 'songs': [line[5]]})
-
-#                 playlist_songs = [line[5]]
-#                 previous_pid = current_pid
-
-                
-# print("----------------------------------------------------- item set list -----------------------------------------------------")
-# print(itemSetList)
 # print("----------------------------------------------------- playlists ---------------------------------------------------------")
 # print(playlists)
 
-# rodamos o algoritmo de fpgrouwth para obter um conjunto de rules
-# itemSetList = [['eggs', 'bacon', 'soup'],
-#             ['eggs', 'bacon', 'apple'],
-#             ['soup', 'bacon', 'banana']]
+for playlist in playlists:
+     itemSetList.append(playlist['songs'])
+                
+# print("----------------------------------------------------- item set list -----------------------------------------------------")
+# print(itemSetList)
 
-freqItemSet, rules = fpgrowth(itemSetList, minSupRatio=0.015, minConf=0.6)
-# print("----------------------------------------------------- frequent item set -----------------------------------------------------")
-# print(freqItemSet)
-# print("----------------------------------------------------- rules -----------------------------------------------------------------")
-# print(rules)
+# rodamos o algoritmo de fpgrouwth para obter um conjunto de rules
+freqItemSet, rules = fpgrowth(itemSetList, minSupRatio=0.1, minConf=0.82)
+print("----------------------------------------------------- rules -----------------------------------------------------------------")
+print(rules)
 
 with open('model.pickle', 'wb') as f:
     pickle.dump(rules, f)
-    # pickle.dump([[{'HUMBLE.'}, {'XO TOUR Llif3'}, 0.6666666666666666], [{'XO TOUR Llif3'}, {'HUMBLE.'}, 0.6153846153846154]], f)
 
+# req = requests.post("http://{}:{}/api/model".format(os.getenv('SERVER_NAME'), os.getenv('SERVER_PORT')), files={'file': open('model.pickle', 'rb')})
 
-req = requests.post("http://{}:{}/api/model".format(os.getenv('SERVER_NAME'), os.getenv('SERVER_PORT')), files={'file': open('model.pickle', 'rb')})
-
-print(req.content)
+# print(req.content)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=37000,debug=True)
--- readme.md
+++ readme.md
@@ -12,7 +12,7 @@ both server and client folders should have a .env configured just like exemplo.e
 
 `docker build . -t tp2-server --no-cache`
 
-`docker run -p 32216:32216 --net tp2-network --name tp2-server-container tp2-server`
+`docker run -p 32216:32216 --name tp2-server-container tp2-server`
 
 ### URL to make requests to server container
 
@@ -27,6 +27,6 @@ http://127.0.0.1:32216/api/recommend
 `docker build . -t tp2-playlist-rules-generator --no-cache`
 
 
-`docker run -p 37000:37000 --net tp2-network --name tp2-playlist-rules-generator-container tp2-playlist-rules-generator`
+`docker run --name tp2-playlist-rules-generator-container tp2-playlist-rules-generator`
 
 client's .env should have the server's container name in SERVER_NAME environment variable and server's port in SERVER_PORT.
