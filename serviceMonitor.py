5efcecd2ea74a21ae67d972dc9d62012c817de8a
Matheus Schimieguel Silva <mschimieguel@gmail.com>
2023-11-27 16:48:25 -0300

    Alteracoes ServiceMonitor

--- serviceMonitor.py
+++ serviceMonitor.py
@@ -1,26 +1,27 @@
 import requests
 import time
 
+
 def make_request():
     try:
         response = requests.get("http://10.109.126.114:32216/api/ping", timeout=0.1)
         response.raise_for_status()
-          
         return response.text
     except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+    
         return None
 
 def main():
     serviceIsDown = 0
     while True:
+        
         result = make_request()
-        print("result: ",result )            
+        print("result: ",result)          
         if  result is None and serviceIsDown == 0: 
             start_time = time.time()
             serviceIsDown = 1
             print("Service Is Down")
-        elif  result == "pong" and serviceIsDown == 1: 
+        if  result == "Pong!" and serviceIsDown == 1: 
             end_time = time.time()
             serviceIsDown = 0
             print("Service Is Up")
