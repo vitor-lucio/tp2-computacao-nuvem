f0abd54f33ddb7184fea2fc3b0f5e4dbf482069d
Matheus Schimieguel Silva <mschimieguel@gmail.com>
2023-11-26 23:45:41 -0300

    alterando deployment

--- wget.sh
+++ wget.sh
@@ -1,6 +1,17 @@
 #teste argocd
+
+#ds2
 wget --server-response \
     --output-document response.out \
     --header='Content-Type: application/json' \
     --post-data '{"songs": ["Black Beatles", "Bounce Back"]}'\
+    http://10.109.126.114:32216/api/recommender
+
+
+#ds1 
+
+wget --server-response \
+    --output-document response.out \
+    --header='Content-Type: application/json' \
+    --post-data '{"songs": ["DNA."]}'\
     http://10.109.126.114:32216/api/recommender
